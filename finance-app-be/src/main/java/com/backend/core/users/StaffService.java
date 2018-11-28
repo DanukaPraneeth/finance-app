fe6d3b7b235c6bac0270df83dbcaaf242e17bd46
tharinduwso2telco <tharindu@wso2telco.com>
2018-11-28 19:34:34 +0530

    Add backend code

---
+++ finance-app-be/src/main/java/com/backend/core/users/StaffService.java
@@ -0,0 +1,67 @@
+package com.backend.core.users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Service
+@Repository
+public class StaffService {
+
+    @Autowired
+    private StaffRepo staffRepo;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    private int i = 0;
+
+/*    public List<Staff> newStaff = new ArrayList<Staff>(Arrays.asList(
+                    new Staff(1,"Rs 750","Bill 1", "string1",2),
+                    new Staff(2,"Rs 578","Bill 2","string 2",3)
+            ));
+*/
+
+    public List<Staff> getAllStaff(){
+
+        List<Staff> staff = new ArrayList<>();
+        staffRepo.findAll()
+                .forEach(staff::add);
+
+        return staff;
+    }
+
+    public Staff getStaff (int id){
+
+        return staffRepo.findOne(id);
+    }
+
+    public void addStaff(Staff employee){
+        employee.setName(String.valueOf(i++));
+        System.out.println(employee.getUserKey());
+        staffRepo.save(employee);
+//        if(employee.getUserKey() != null){
+//            employee = em.merge(employee);
+//        }
+//        em.persist(employee);
+    }
+
+    public void updateStaff (int id , Staff employee){
+        staffRepo.save(employee);
+    }
+
+    public void remveStaff (int id){
+        staffRepo.delete(id);
+    }
+
+    public List<Staff> getByName (String name){return staffRepo.findByName(name);}
+
+
+}
